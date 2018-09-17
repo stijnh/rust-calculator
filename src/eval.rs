27d78f50ae388588bee612fc9ed892fbe07448b1
stijnh <mail@stijnh.nl>
2018-09-17 13:37:32 +0200

    Modified the way comparison are performed.

--- src/eval.rs
+++ src/eval.rs
@@ -1,18 +1,24 @@
 use parser::{Node, Op};
 use std::collections::HashMap;
-use std::fmt;
 use std::rc::Rc;
+use std::{cmp, fmt};
 
 pub trait Func {
     fn name(&self) -> Option<&str>;
     fn call(&self, args: &Vec<Value>) -> Result<Value, EvalError>;
 }
 
-#[derive(Clone)]
+#[derive(PartialEq, Clone)]
 pub enum Value {
     Number(f64),
     Boolean(bool),
     Function(Rc<Box<Func>>),
+}
+
+impl PartialEq for Func {
+    fn eq(&self, other: &Func) -> bool {
+        return self as *const Func == other as *const Func;
+    }
 }
 
 impl Value {
@@ -48,6 +54,20 @@ impl fmt::Debug for Value {
             Value::Number(x) => write!(f, "Number({:?})", x),
             Value::Boolean(x) => write!(f, "Boolean({:?})", x),
             Value::Function(_) => write!(f, "Function(...)"),
+        }
+    }
+}
+
+impl cmp::PartialOrd for Value {
+    fn partial_cmp(&self, other: &Value) -> Option<cmp::Ordering> {
+        if self == other {
+            return Some(cmp::Ordering::Equal);
+        }
+
+        match (self, other) {
+            (Value::Number(x), Value::Number(y)) => x.partial_cmp(y),
+            (Value::Boolean(x), Value::Boolean(y)) => Some(x.cmp(y)),
+            _ => None,
         }
     }
 }
@@ -93,41 +113,40 @@ impl<'a> Context<'a> {
 }
 
 fn compare_values(op: Op, lhs: &Value, rhs: &Value) -> Option<bool> {
+    use Value::Boolean as B;
+    use Value::Number as N;
+
     match op {
         Op::Gt => compare_values(Op::Lt, rhs, lhs),
         Op::Gte => compare_values(Op::Lte, rhs, lhs),
         Op::Neq => compare_values(Op::Eq, lhs, rhs).map(|b| !b),
-        Op::Lte => 
-            if let Some(true) = compare_values(Op::Eq, lhs, rhs) {
+        Op::Lte => {
+            if lhs == rhs {
                 Some(true)
             } else {
                 compare_values(Op::Lt, lhs, rhs)
-            },
-        Op::Eq => {
-            match (lhs, rhs) {
-                (Value::Number(x), Value::Number(y)) => Some(x == y),
-                (Value::Boolean(x), Value::Boolean(y)) => Some(x == y),
-                _ => None
             }
-        },
-        _ => None
+        }
+        Op::Eq => Some(lhs == rhs),
+        Op::Lt => lhs.partial_cmp(rhs).map(|x| x == cmp::Ordering::Less),
+        _ => None,
     }
 }
 
 fn evaluate_binop(op: Op, lhs: &Value, rhs: &Value) -> Result<Value, EvalError> {
-    use Value::Number as N;
     use Value::Boolean as B;
+    use Value::Number as N;
 
     if let Some(b) = compare_values(op, lhs, rhs) {
         return Ok(B(b));
     }
 
     match (op, lhs.as_bool()) {
-        (Op::And, Some(true))  => return Ok(rhs.clone()),
+        (Op::And, Some(true)) => return Ok(rhs.clone()),
         (Op::And, Some(false)) => return Ok(lhs.clone()),
-        (Op::Or, Some(true))  => return Ok(lhs.clone()),
+        (Op::Or, Some(true)) => return Ok(lhs.clone()),
         (Op::Or, Some(false)) => return Ok(rhs.clone()),
-        _ => ()
+        _ => (),
     }
 
     let out = match (op, lhs.clone(), rhs.clone()) {
@@ -135,16 +154,16 @@ fn evaluate_binop(op: Op, lhs: &Value, rhs: &Value) -> Result<Value, EvalError> 
         (Op::Sub, N(x), N(y)) => N(x - y),
         (Op::Mul, N(x), N(y)) => N(x * y),
         (Op::Div, N(x), N(y)) => N(x / y),
-        (Op::Eq, N(x), N(y)) =>  B(x == y),
-        (Op::Lt, N(x), N(y)) =>  B(x < y),
 
         (Op::Mul, B(x), B(y)) => B(x && y),
         (Op::Add, B(x), B(y)) => B(x || y),
-        (Op::Eq, B(x), B(y)) =>  B(x == y),
-        (Op::Lt, B(x), B(y)) =>  B(x < y),
         _ => {
-            return Err(EvalError(format!("invalid binary operator '{}' for types {} and {}",
-                                         op.name(), lhs.type_name(), rhs.type_name())));
+            return Err(EvalError(format!(
+                "invalid binary operator '{}' for types {} and {}",
+                op.name(),
+                lhs.type_name(),
+                rhs.type_name()
+            )));
         }
     };
 
@@ -152,8 +171,8 @@ fn evaluate_binop(op: Op, lhs: &Value, rhs: &Value) -> Result<Value, EvalError> 
 }
 
 fn evaluate_monop(op: Op, arg: &Value) -> Result<Value, EvalError> {
-    use Value::Number as N;
     use Value::Boolean as B;
+    use Value::Number as N;
 
     let out = match (op, arg.clone()) {
         (Op::Add, N(x)) => N(x),
@@ -161,8 +180,11 @@ fn evaluate_monop(op: Op, arg: &Value) -> Result<Value, EvalError> {
         (Op::Not, N(x)) => B(x == 0.0),
         (Op::Not, B(x)) => B(!x),
         _ => {
-            return Err(EvalError(format!("invalid unary operator '{}' for type {}",
-                                         op.name(), arg.type_name())));
+            return Err(EvalError(format!(
+                "invalid unary operator '{}' for type {}",
+                op.name(),
+                arg.type_name()
+            )));
         }
     };
 
@@ -172,7 +194,12 @@ fn evaluate_monop(op: Op, arg: &Value) -> Result<Value, EvalError> {
 fn evaluate_apply(fun: &Value, args: &Vec<Value>) -> Result<Value, EvalError> {
     let fun = match fun {
         Value::Function(f) => f,
-        _ => return Err(EvalError(format!("invalid cast to function"))),
+        _ => {
+            return Err(EvalError(format!(
+                "value of type {} is not callable",
+                fun.type_name()
+            )))
+        }
     };
 
     fun.call(args)
--- src/funcs.rs
+++ src/funcs.rs
@@ -1,9 +1,41 @@
+extern crate rand;
+
+use self::rand::random;
 use eval::{Context, EvalError, Func, Value};
 use std::f64::consts;
 use std::rc::Rc;
 
 fn set_const(ctx: &mut Context, key: &str, val: f64) {
     ctx.set(key, Value::Number(val))
+}
+
+fn check_num_args(args: &Vec<Value>, want: usize) -> Result<(), EvalError> {
+    let (x, y) = (args.len(), want);
+
+    if x != y {
+        let buffer = format!(
+            "expected {} argument{}, {} argument{} given",
+            x,
+            if x == 1 { "" } else { "s" },
+            y,
+            if y == 1 { "" } else { "s" }
+        );
+
+        Err(EvalError(buffer))
+    } else {
+        Ok(())
+    }
+}
+
+fn check_number(arg: &Value) -> Result<f64, EvalError> {
+    if let Some(x) = arg.as_number() {
+        Ok(x)
+    } else {
+        Err(EvalError(format!(
+            "invalid cast of {} to number",
+            arg.type_name()
+        )))
+    }
 }
 
 struct ClosureFunction<F>(String, F);
@@ -20,50 +52,38 @@ where
     }
 }
 
+fn set_closure<F: 'static>(ctx: &mut Context, key: &str, fun: F)
+where
+    F: Fn(&Vec<Value>) -> Result<Value, EvalError>,
+{
+    let f = ClosureFunction(key.to_string(), fun);
+
+    ctx.set(key, Value::Function(Rc::new(Box::new(f))));
+}
+
 fn set_unary<F: 'static>(ctx: &mut Context, key: &str, fun: F)
 where
     F: Fn(f64) -> f64,
 {
-    let f = ClosureFunction(key.to_string(), move |args: &Vec<Value>| {
-        if args.len() != 1 {
-            return Err(EvalError(format!(
-                "expected 1 argument, {} arguments given",
-                args.len()
-            )));
-        }
-
-        let x = match args[0].as_number() {
-            Some(x) => x,
-            _ => return Err(EvalError("invalid cast to number".into())),
-        };
+    set_closure(ctx, key, move |args: &Vec<Value>| {
+        check_num_args(args, 1)?;
+        let x = check_number(&args[0])?;
 
         Ok(Value::Number(fun(x)))
     });
-
-    ctx.set(key, Value::Function(Rc::new(Box::new(f))));
 }
 
 fn set_binary<F: 'static>(ctx: &mut Context, key: &str, fun: F)
 where
     F: Fn(f64, f64) -> f64,
 {
-    let f = ClosureFunction(key.to_string(), move |args: &Vec<Value>| {
-        if args.len() != 2 {
-            return Err(EvalError(format!(
-                "expected 1 argument, {} arguments given",
-                args.len()
-            )));
-        }
-
-        let (x, y) = match (args[0].as_number(), args[1].as_number()) {
-            (Some(x), Some(y)) => (x, y),
-            _ => return Err(EvalError("invalid cast to number".into())),
-        };
+    set_closure(ctx, key, move |args: &Vec<Value>| {
+        check_num_args(args, 2)?;
+        let x = check_number(&args[0])?;
+        let y = check_number(&args[1])?;
 
         Ok(Value::Number(fun(x, y)))
     });
-
-    ctx.set(key, Value::Function(Rc::new(Box::new(f))));
 }
 
 pub fn create() -> Context<'static> {
@@ -73,10 +93,10 @@ pub fn create() -> Context<'static> {
         let c = &mut ctx;
         set_const(c, "pi", consts::PI);
         set_const(c, "e", consts::E);
-        set_const(c, "e", consts::E);
 
         set_unary(c, "sin", |x| x.sin());
         set_unary(c, "cos", |x| x.cos());
+        set_unary(c, "tan", |x| x.tan());
         set_unary(c, "ln", |x| x.ln());
         set_unary(c, "log10", |x| x.log10());
         set_unary(c, "log2", |x| x.log2());
@@ -90,6 +110,34 @@ pub fn create() -> Context<'static> {
 
         set_binary(c, "pow", |x, y| x.powf(y));
         set_binary(c, "log", |x, y| x.log(y));
+        set_binary(c, "hypot", |x, y| x.hypot(y));
+
+        set_closure(c, "max", |args| {
+            if args.len() == 0 {
+                return Err(EvalError("max of empty sequence".into()));
+            }
+
+            let mut best = args[0].clone();
+
+            for arg in args {
+                if arg > &best {
+                    best = arg.clone();
+                }
+            }
+
+            Ok(best)
+        });
+
+        set_closure(c, "rand", |args| {
+            let (a, b) = match args.len() {
+                0 => (0.0, 1.0),
+                1 => (0.0, check_number(&args[0])?),
+                _ => (check_number(&args[0])?, check_number(&args[1])?),
+            };
+
+            let out = (b - a) * random::<f64>() + a;
+            Ok(Value::Number(out))
+        });
     }
 
     ctx
